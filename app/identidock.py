7324336d100509ffb4c5378672580062a3e763ab
Tanvir Tanviruzzaman <motanv@microsoft.com>
2018-01-24 06:56:52 -0800

    preparing for jenkins

--- app/identidock.py
+++ app/identidock.py
@@ -2,17 +2,16 @@ from flask import Flask, Response, request
 import requests
 import hashlib
 import redis
+import html
 app = Flask(__name__)
 cache = redis.StrictRedis(host='redis', port=6379, db=0)
 salt = "UNIQUE_SALT"
-default_name = 'Arpi Zaman'
-
+default_name = 'Joe Bloggs'
 @app.route('/', methods=['GET', 'POST'])
 def mainpage():
   name = default_name
   if request.method == 'POST':
-    name = request.form['name']
-
+    name = html.escape(request.form['name'], quote=True)
   salted_name = salt + name
   name_hash = hashlib.sha256(salted_name.encode()).hexdigest()
   header = '<html><head><title>Identidock</title></head><body>'
@@ -28,14 +27,13 @@ def mainpage():
 
 @app.route('/monster/<name>')
 def get_identicon(name):
+  name = html.escape(name, quote=True)
   image = cache.get(name)
   if image is None:
     print ("Cache miss", flush=True)
     r = requests.get('http://dnmonster:8080/monster/' + name + '?size=80')
     image = r.content
     cache.set(name, image)
-
   return Response(image, mimetype='image/png')
-
 if __name__ == '__main__':
   app.run(debug=True, host='0.0.0.0')
